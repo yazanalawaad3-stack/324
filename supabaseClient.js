dda576eaf1096ff0c8c5c00d39e8ba3c4435d940
yazanalawaad3-stack <yazanalawaad3@gmail.com>
2026-02-26 07:03:44 +0200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-export const SUPABASE_URL = 'https://lcwppbfjzhyyskdbzahp.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxjd3BwYmZqemh5eXNrZGJ6YWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODYwMDksImV4cCI6MjA4NzU2MjAwOX0.PWyYHrvyqP2jeCKsumbBwiyMfGVNXYpWnOQFwlmpp9Q';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
